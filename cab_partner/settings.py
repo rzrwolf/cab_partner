2cf9ab7ffde1e880bee66be44cab46acf973bb9e
rzr <rzrwolf@ya.ru>
2023-05-09 11:43:14 +0300

    login form

--- cab_partner/settings.py
+++ cab_partner/settings.py
@@ -10,10 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+PROJECT_DIR = os.path.join(BASE_DIR, os.pardir)
+
+
+TENANT_APPS_DIR = os.path.join(PROJECT_DIR, os.pardir)
+sys.path.insert(0, TENANT_APPS_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +33,6 @@ SECRET_KEY = 'django-insecure-wj0w-%*k_)ie4x__1&mj@idjqfp&-(ro-(9(3d7zt5%*mhhmti
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -53,42 +59,62 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_tenants.middleware.main.TenantMainMiddleware',
 ]
-
-ROOT_URLCONF = 'cab_partner.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
-        'APP_DIRS': True,
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+
+        ],
+        # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'loaders': (
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ),
+
         },
     },
 ]
+ROOT_URLCONF = 'cab_partner.urls'
+PUBLIC_SCHEMA_URLCONF = 'cab_partner.urls_public'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 WSGI_APPLICATION = 'cab_partner.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django_tenants.postgresql_backend',
         'NAME': 'cab-partner',
         'USER': 'postgres',
         'PASSWORD': 'rzr',
     }
 }
-
+DATABASE_ROUTERS = (
+    'django_tenants.routers.TenantSyncRouter',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -108,7 +134,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -120,14 +145,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, '/media/static/')
+STATIC_URL = 'media/static/'
+
+
 
 STATICFILES_DIRS = [
-    BASE_DIR / "media/static/"
+    os.path.join(BASE_DIR, 'media/static/')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
@@ -139,3 +165,40 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+SHARED_APPS = (
+    'cab_partner',  # mandatory
+    'clients',  # you must list the app where your tenant model resides in
+
+    'django.contrib.contenttypes',
+
+    # everything below here is optional
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
+    'django_tenants',
+)
+
+TENANT_APPS = (
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    # your tenant-specific apps
+    'probes',
+    'orders',
+    'patients',
+)
+
+INSTALLED_APPS = list(SHARED_APPS) + [app for app in TENANT_APPS if app not in SHARED_APPS]
+
+TENANT_MODEL = "clients.Client"  # app.Model
+
+TENANT_DOMAIN_MODEL = "clients.Domain"  # app.Model
+SHOW_PUBLIC_IF_NO_TENANT_FOUND = False
+PUBLIC_SCHEMA_NAME = 'public'
+
+DEFAULT_FILE_STORAGE = "django_tenants.files.storage.TenantFileSystemStorage"
--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django_tenants.admin import TenantAdminMixin
+
+from clients.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
+        list_display = ('name', 'created_on')
